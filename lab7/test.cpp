c9a3a86bdb4687a2d3dbe03d73a4337e68b4c596
Ilya Lazuk <36516154+LaRtik@users.noreply.github.com>
2021-06-01 16:39:52 +0300

    Add files via upload

--- lab7/test.cpp
+++ lab7/test.cpp
@@ -11,6 +11,13 @@ void test_list_add()
 	assert(temp.size == 1);
 	vector <HotelTracer::HotelInfo> disp = temp.get();
 	assert(disp[0].city == "Grodno");
+
+	temp2.city = "Minsk";
+	temp.push(temp2);
+	disp = temp.get();
+	assert(temp.size == 2);
+	assert(disp[0].city == "Grodno");
+	assert(disp[1].city == "Minsk");
 }
 
 #undef main
